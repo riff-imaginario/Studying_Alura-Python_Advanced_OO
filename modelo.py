0304eeebc28c6c260b3b4f8a79071168682f3bbd
Lucas Nogueira <riff_imaginario@localhost.localdomain>
2019-08-22 16:12:44 -0300

    add property and setter

--- modelo.py
+++ modelo.py
@@ -1,25 +1,49 @@
 class Filme:
     
     def __init__(self, nome, ano, duracao):
-        self.nome    = nome.title()
+        self.__nome    = nome.title()
         self.ano     = ano
         self.duracao = duracao
-        self.like    = 0
+        self.__like    = 0
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+
+    @property
+    def likes(self):
+         return self.__likes
 
     def dar_like(self):
-        self.like += 1
+        self.__like += 1
 
 
 class Serie:
     
     def __init__(self, nome, ano, temporadas):
-        self.nome       = nome.title()
+        self.__nome       = nome.title()
         self.ano        = ano
         self.temporadas = temporadas
-        self.like    = 0
+        self.__like    = 0
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+
+    @property
+    def likes(self):
+         return self.__likes
 
     def dar_like(self):
-        self.like += 1
+        self.__like += 1
 
 vingadores = Filme('Vingadores - Guerra Infinita', 2018, 160)
 
@@ -28,6 +52,6 @@ atlanta = Serie('Atlanta', 2018, 2)
 vingadores.dar_like()
 atlanta.dar_like()
 
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duracao: {vingadores.duracao} - Likes: {vingadores.like}')
+print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duracao: {vingadores.duracao} - Likes: {vingadores.likes}')
 
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas} - Likes: {atlanta.like}')
+print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}')
