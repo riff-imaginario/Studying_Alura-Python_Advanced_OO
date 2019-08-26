ab15e6f658cd8590fa9d3913d29b6253eff1bcc3
Lucas Nogueira <riff_imaginario@localhost.localdomain>
2019-08-26 15:13:30 -0300

    add method imprime()

--- modelo.py
+++ modelo.py
@@ -29,6 +29,24 @@ class Filme(Programa):
         self.duracao = duracao
         self._like   = 0
 
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+
+    @property
+    def likes(self):
+         return self._like
+
+    def dar_like(self):
+        self._like += 1
+
+    def imprime(self):
+        print(f'Nome: {self._nome} - Ano: {self.ano} - Duracao: {self.duracao} - Likes: {self._like}')
+
 
 # Serie herda de Programa
 class Serie(Programa):
@@ -52,22 +70,18 @@ class Serie(Programa):
          return self.__like
 
     def dar_like(self):
-        self.__like += 1
+        self._like += 1
+
+    def imprime(self):
+        print(f'Nome: {self._nome} - Ano: {self.ano} - Temporadas: {self.temporadas} - Likes: {self._like}')
 
 vingadores = Filme('Vingadores - Guerra Infinita', 2018, 160)
-
-atlanta = Serie('Atlanta', 2018, 2)
+atlanta    = Serie('Atlanta', 2018, 2)
 
 vingadores.dar_like()
 atlanta.dar_like()
 
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duracao: {vingadores.duracao} - Likes: {vingadores.likes}')
-
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}')
-
-
 filmes_e_series = [vingadores, atlanta]
 
 for programa in filmes_e_series:
-    detalhes = programa.duracao if hasattr(programa, 'duracao') else programa.temporadas
-    print(f'{programa.nome} - {detalhes} - {programa.likes}')
+    programa.imprime()
